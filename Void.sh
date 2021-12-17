e28994a2f85f2f945d8b77e747b3ed6a496dce1c
Maharsh17 <janimaharsh@gmail.com>
2021-12-17 22:03:28 +0530

    Test

--- Void.sh
+++ Void.sh
@@ -14,63 +14,61 @@
  sudo xbps-install xorg xf86-video-amdgpu mesa xf86-video-ati
 
  # -- Terminal -- #
- sudo xbps-install alacritty kitty
+ sudo xbps-install alacritty kitty zsh
 
  # -- WM -- #
  sudo xbps-install bspwm sxhkd polybar rofi picom
 
  # -- Other -- #
- sudo xbps-install xtools nitrogen git wget curl tar unzip
-
- # -- Fonts -- #
-
- VER="v2.1.0"
- mkdir User
- cd User
-
-  # -- Source Code Pro -- # 
-  wget https://github.com/ryanoasis/nerd-fonts/releases/download/$VER/SourceCodePro.zip 
-  unzip SourceCodePro.zip
-  rm -rf SourceCodePro.zip
+ sudo xbps-install xtools nitrogen git wget curl tar unzip lxappearance
 
   # -- Jet Brains Mono -- #
-  wget https://github.com/ryanoasis/nerd-fonts/releases/download/$VER/JetBrainsMono.zip
-  unzip JetBrainsMono.zip
-  rm -rf JetBrainsMono.zip
+  wget https://download.jetbrains.com/fonts/JetBrainsMono-2.242.zip
+  unzip JetBrainsMono-*.zip
+  rm -rf *.zip 
+  rm rm -rf AUTHORS.txt OFL.txt fonts/variable fonts/webfonts
+  cd fonts
+  mkdir -p ~/.local/share/fonts
+  mv ttf ~/.local/share/fonts
+  cd .. 
+  fc-cache -f -v
 
-  # -- Hack -- #
-  wget https://github.com/ryanoasis/nerd-fonts/releases/download/$VER/Hack.zip
-  unzip Hack.zip
-  rm -rf Hack.zip
 
- cd ..
- mv User /usr/share/fonts/
- fc-cache -f -v
 
 # -- Configure WM -- #
 
  # -- BSPWM -- #
  git clone https://github.com/Maharsh17/Dot.git
  cd Dot
- rm README.md .xinitrc
- rm -rf polybar/ screenshot/
- mv bspwm sxhkd ~/.config/
+ rm README.md
+
+ mv bspwm sxhkd polybar alacritty kitty ~/.config/
  chmod +x ~/.config/bspwm/bpswmrc
+
  mkdir ~/Wall
  mv wallpaper/Wall.jpg ~/Wall/
- mv alacritty/ ~/.config/ 
- mv .bashrc ~/
- rm -rf *
- cd ..
 
- # -- Polybar -- #
- git clone https://github.com/adi1090x/polybar-themes
- cd polybar-themes
- ./setup.sh
+ mv .gtkrc-2.0 ~/
+ mv .bashrc ~/
+ mv .xinitrc ~/
+ rm -rf *
  cd ..
 
  # -- Starship Prompt -- #
  sh -c "$(curl -fsSL https://starship.rs/install.sh)"
+
+ # -- ZSH -- #
+ sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+ rm ~/.zshrc
+ mv .zshrc ~/ 
+
+ # -- SHELL -- #
+ chsh -s /usr/bin/zsh
+
+ # -- GTK -- #
+ git clone https://github.com/dracula/gtk Dracula
+ mkdir ~/.themes
+ mv Dracula ~/.themes
 
 # -- Update -- #
  sudo xbps-install -Suv
